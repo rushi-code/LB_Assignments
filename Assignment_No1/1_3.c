f1f9c46e42af2795d1e787e9cd3302542c1220ae
Rushikesh Ingale <127707145+rushi-code@users.noreply.github.com>
2023-04-22 23:45:25 +0530

    Assignment_No1

---
+++ Assignment_No1/1_3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void Display()
+{
+    int i =0;
+   
+    i=5;
+    for(i=5;i>=1;i--)
+    {
+        printf("%d\t",i);
+       
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
