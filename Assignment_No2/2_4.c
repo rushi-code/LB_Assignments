92621fabec6fc352b2895f698b2da253ead957fd
Rushikesh Ingale <127707145+rushi-code@users.noreply.github.com>
2023-05-04 19:49:05 +0530

    Assignment_No2

---
+++ Assignment_No2/2_4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void Display(int iNo,int iFrequency)
+{
+    if(iFrequency<0)
+    {
+        iFrequency = -iFrequency;
+    }
+    int icnt = 0;
+    for(icnt = 0; icnt<iFrequency; icnt++)
+    {
+        printf("%d\t",iNo);
+    }
+}
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+    printf("Enter Number\n");
+    scanf("%d",&iValue);
+
+    printf("Enter Frequency\n");
+    scanf("%d",&iCount);
+
+    Display(iValue,iCount);
+
+    
+    return 0;
+}
