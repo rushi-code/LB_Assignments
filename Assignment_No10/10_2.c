86099efcb7634d44e2ed11fc109438777a36079d
Rushikesh Ingale <127707145+rushi-code@users.noreply.github.com>
2023-05-11 21:51:00 +0530

    Assignment_No10

---
+++ Assignment_No10/10_2.c
@@ -0,0 +1,62 @@
+//Accept N numbers from user and return difference between frequency of even number and odd number
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Freq(int Arr[],int iLength)
+{
+    int iCnt = 0;
+    int iCounterE = 0;
+    int iCounterO = 0;
+    for(iCnt =0; iCnt<iLength;iCnt++)
+    {
+        if((Arr[iCnt]%2)==0)
+        {
+            iCounterE++;
+        }
+    }
+
+    for(iCnt =0; iCnt<iLength;iCnt++)
+    {
+        if((Arr[iCnt]%2)!=0)
+        {
+            iCounterO++;
+        }
+    }
+    return(iCounterE-iCounterO);
+    
+}
+
+int main()
+{
+    int iSize = 0;
+    int iCnt = 0;
+    int iRet = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements\n");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize*sizeof(int));
+
+    if(p==NULL)
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter %d elements\n",iSize);
+    for(iCnt = 0; iCnt<iSize;iCnt++)
+    {
+        printf("Enter element %d:",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Freq(p,iSize);
+    printf("Difference between freq is %d\n",iRet);
+
+    free(p);
+
+    return 0;
+
+}
