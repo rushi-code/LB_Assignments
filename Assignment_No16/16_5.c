f3c54f2881a68f9d1afca72316410a578b971a74
Rushikesh Ingale <127707145+rushi-code@users.noreply.github.com>
2023-05-16 00:29:12 +0530

    Assignment_No16

---
+++ Assignment_No16/16_5.c
@@ -0,0 +1,40 @@
+//Accept number of rows and columns from user and display below pattern
+
+/*
+    Input :
+    iRow = 3    iCol = 4
+
+    Output :
+    1   2   3   4
+    5   6   7   8   
+    9   10  11  12
+*/
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+    int i, j;
+    int num = 1;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("%d\t", num);
+            num++;
+        }
+        printf("\n");
+    }
+}
+
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows and columns\n");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+    return 0;
+}
